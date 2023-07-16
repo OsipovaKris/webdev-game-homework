c470e1abdb50c3530d2c35b574a3f11e24721d3a
OsipovaKris <Osipova.svs@mail.ru>
2023-07-16 17:08:21 +0300

    Верстка первой страницы

--- index.js
+++ index.js
@@ -1,18 +1,2 @@
 const appEl = document.querySelector('.game-container');
-const gameLevel = document.querySelectorAll('.game-level');
 const gameInfoBtn = document.querySelector('.game-info-btn');
-
-
-for (let i = 0; i < gameLevel.length; i++) {
-
-    gameLevel[i].addEventListener('click', function (e) {
-
-        for (let i = 0; i < gameLevel.length; i++) {
-
-            gameLevel[i].classList.remove('active')
-        }
-
-        e.target.classList.add('active');
-    })
-};
-

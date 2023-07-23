e1036b62cb495e5058bd0da37b3eb362202302a7
OsipovaKris <Osipova.svs@mail.ru>
2023-07-23 20:03:28 +0300

    Настроены линтеры и форматирование

---
+++ index/index.js
@@ -0,0 +1,13 @@
+const appEl = document.querySelector(".game-container");
+const gameInfoBtn = document.querySelector(".game-info-btn");
+const gameLevel = document.querySelectorAll("input");
+
+gameInfoBtn.addEventListener("click", function () {
+  for (let i = 0; i < gameLevel.length; i++) {
+    if (gameLevel[i].checked) {
+      appEl.innerHTML = `Здесь будет ${gameLevel[i].value} уровень`;
+
+      // localStorage.setItem('level', `${gameLevel[i].value}`);
+    }
+  }
+});

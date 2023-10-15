968086dde60071094d8dac6be50ae4b3cd7b5cce
OsipovaKris <Osipova.svs@mail.ru>
2023-10-15 20:39:46 +0300

    Создание отдельного файла для функции

---
+++ components/generate-card.ts
@@ -0,0 +1,16 @@
+import { state, cardBoxArr } from "../index/index";
+
+export function generateCard(cardBoxArr: string[]) {
+  const generateCardArr = (length: number, max: number) =>
+    [...new Array(length)].map(() => Math.round(Math.random() * max));
+
+  const cardArr = generateCardArr(state.level * 3, 35);
+
+  const allCardArr = cardArr.concat(cardArr).sort(() => Math.random() - 0.5);
+
+  for (let i = 0; i < state.level * 6; i++) {
+    cardBoxArr.push(
+      `<div data-id=${allCardArr[i]} class='game-card' style='background-image: url("../static/img/${allCardArr[i]}.svg");'></div>`,
+    );
+  }
+}
